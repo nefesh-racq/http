dccd32ecc0cddc640556c66bdb8523a8e5817e60
epsilon <nefesh_racq_@hotmail.com>
2019-03-05 01:05:59 -0400

    insert funcionando

--- app/src/main/java/com/example/epsilon/http/MainActivity.java
+++ app/src/main/java/com/example/epsilon/http/MainActivity.java
@@ -3,17 +3,65 @@ package com.example.epsilon.http;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etxtLatitud;
+    private EditText etxtLongitud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etxtLatitud = (EditText)findViewById(R.id.etxtLatitud);
+        etxtLongitud = (EditText)findViewById(R.id.etxtLongitud);
+
     }
 
     public void btnGet(View view) {
-
+        executeService("http://192.168.1.105/AppWeb/insert.php");
     }
 
+    private void executeService(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), "OPERACION EXITOSA..", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+
+                params.put("x", etxtLatitud.getText().toString());
+                params.put("y", etxtLongitud.getText().toString());
+
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    public void btnSet(View view) {
+    }
 }
