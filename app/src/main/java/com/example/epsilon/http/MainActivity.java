e5b4ade19fbeb5f99781641c1c6b99d58f375ac3
epsilon <nefesh_racq_@hotmail.com>
2019-03-04 21:52:21 -0400

    inicio, diseño

---
+++ app/src/main/java/com/example/epsilon/http/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.epsilon.http;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnGet(View view) {
+
+    }
+
+}
